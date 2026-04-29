2a3560b57e7e762c1fb9ba644e290e9e96f6552c
varshini <sri@gmail.com>
2026-04-29 11:09:33 +0530

    updated render config

--- src/main/java/com/klef/fsad/service/GoogleService.java
+++ src/main/java/com/klef/fsad/service/GoogleService.java
@@ -1,6 +1,7 @@
 package com.klef.fsad.service;
 
 import com.google.api.client.googleapis.auth.oauth2.*;
+
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import java.util.Collections;
 @Service
 public class GoogleService {
 
-    private static final String CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID";
+    private static final String CLIENT_ID = "11180153759-5fhuog9334cns14c25d069aihgpo2hu7.apps.googleusercontent.com";
 
     public GoogleIdToken.Payload verifyToken(String token) throws Exception {
 
